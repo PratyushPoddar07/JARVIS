63001879fb77996d4fe8f9dd6cf8914ae380b392
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-08-07 21:12:06 +0530

    jarvis-3

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# playing jarvis assistant sound
+def playAssistantsound():
+    music_dir = "www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 
 import eel
 
+from engine.features import *
 eel.init("www") # to recognize our frontend directory
 
+playAssistantsound() # to play the assistant sound
 os.system('start msedge.exe --app="http://localhost:8000/index.html"') # to open the browser
 eel.start("index.html",mode="none",host="localhost",block=True) # to start the eel application
