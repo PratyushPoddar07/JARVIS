ae34c5eb1ed94d76d2d395bbfa830e497c9f6c27
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-08-07 21:35:19 +0530

    jarvis-4

--- engine/features.py
+++ engine/features.py
@@ -1,6 +1,7 @@
 from playsound import playsound
-
+import eel
 # playing jarvis assistant sound
+@eel.expose
 def playAssistantsound():
     music_dir = "www\\assets\\audio\\start_sound.mp3"
     playsound(music_dir)
